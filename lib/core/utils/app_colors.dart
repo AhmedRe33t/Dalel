a765f92dab9ca34f02573177870e2af37f0b771c
ahmedrefaat <arrefaat171@gmail.com>
2024-08-28 01:34:16 +0300

    constant

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+abstract class AppColors{
+  static Color primaryColor=const Color(0xffC49E85);
+    static Color offWhite=const Color(0xffF8F4F9);
+        static Color depBrown=const Color(0xff6B4B3E);
+        static Color depGrey=const Color(0xff6F6460);
+        static Color lightGrey=const Color(0xffB4B4B4);
+        static Color blak=const Color(0xff333333);
+
+
+}
