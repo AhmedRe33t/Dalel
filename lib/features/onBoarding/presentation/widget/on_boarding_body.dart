3549e4e8b12b58ac84503e6ec97f04d3caa82033
ahmedrefaat <arrefaat171@gmail.com>
2024-08-28 04:33:05 +0300

    create on boarding model

--- lib/features/onBoarding/presentation/widget/on_boarding_body.dart
+++ lib/features/onBoarding/presentation/widget/on_boarding_body.dart
@@ -1,5 +1,6 @@
 import 'package:dalelapp/core/utils/app_assets.dart';
 import 'package:dalelapp/core/utils/app_text_styles.dart';
+import 'package:dalelapp/features/onBoarding/data/models/on_boarding_model.dart';
 import 'package:dalelapp/features/onBoarding/presentation/widget/custom_smooth_page.dart';
 import 'package:flutter/material.dart';
 
@@ -15,15 +16,15 @@ class onBoarding extends StatelessWidget {
       child: PageView.builder(
         physics:const BouncingScrollPhysics(),
         controller: _controller,
-        itemCount: 3,
+        itemCount: onBoardingData.length,
         itemBuilder:(context, index) {
         return Column(
           children: [
            Container(
             height: 290,
             width: 343,
-            decoration:const BoxDecoration(
-              image: DecorationImage(image: AssetImage(Assets.imagesOnBoarding1),fit: BoxFit.fill),
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(onBoardingData[index].image),fit: BoxFit.fill),
               
             ),
             
@@ -31,14 +32,14 @@ class onBoarding extends StatelessWidget {
         const SizedBox(height: 24,),
             CustomSmoothIndecator(controller:_controller),
            const SizedBox(height: 32,),
-            Text("Explore The history with Dalel in a smart way",style: CustomTextStyles.poppins500style24.copyWith(fontWeight: FontWeight.bold,
+            Text(onBoardingData[index].title,style: CustomTextStyles.poppins500style24.copyWith(fontWeight: FontWeight.bold,
             ),
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
 
             textAlign: TextAlign.center),
             const SizedBox(height: 16,),
-        const    Text("Explore The history with Dalel in a smart way",style: CustomTextStyles.poppins300style16,
+            Text(onBoardingData[index].subTitle,style: CustomTextStyles.poppins300style16,
             textAlign: TextAlign.center)
             
           ],
